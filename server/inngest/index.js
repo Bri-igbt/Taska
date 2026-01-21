a255ecd63bdee4e3ed0561e2b7290837c2341c88
ChimaBright <blakechima@gmail.com>
2026-01-21 14:21:55 +0100

    vercel setup

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -37,5 +37,29 @@ const deleteUserData = inngest.createFunction(
     }
 )
 
+// Inngest function to update user data in database
+const updateUserData = inngest.createFunction(
+    { id: "update-user-with-clerk"},
+    { event: "clerk/user.updated" },
+    async ({ event }) => {
+        const { data } = event
+        // Simulate updating user data in database
+        await prisma.user.updateMany({
+            where: {
+                id: data.id,
+            },
+            data: {
+                email: data.email_addresses[0]?.email_address,
+                name: data.first_name + ' ' + data.last_name,
+                image: data?.image_url
+            },
+        })
+    }
+)
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    saveUserData,
+    deleteUserData,
+    updateUserData
+];
