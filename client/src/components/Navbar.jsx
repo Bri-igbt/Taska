800db04b9acdb289995a1e650387d5571147a585
ChimaBright <blakechima@gmail.com>
2026-03-27 17:34:01 +0100

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,14 +13,11 @@ const Navbar = ({ setIsSidebarOpen }) => {
     return (
         <div className="w-full bg-white dark:bg-zinc-900 border-b border-gray-200 dark:border-zinc-800 px-6 xl:px-16 py-3 flex-shrink-0">
             <div className="flex items-center justify-between max-w-6xl mx-auto">
-                {/* Left section */}
                 <div className="flex items-center gap-4 min-w-0 flex-1">
-                    {/* Sidebar Trigger */}
                     <button onClick={() => setIsSidebarOpen((prev) => !prev)} className="sm:hidden p-2 rounded-lg transition-colors text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-zinc-800" >
                         <PanelLeft size={20} />
                     </button>
 
-                    {/* Search Input */}
                     <div className="relative flex-1 max-w-sm">
                         <SearchIcon className="absolute left-2.5 top-1/2 -translate-y-1/2 text-gray-400 dark:text-zinc-400 size-3.5" />
                         <input
@@ -31,10 +28,7 @@ const Navbar = ({ setIsSidebarOpen }) => {
                     </div>
                 </div>
 
-                {/* Right section */}
                 <div className="flex items-center gap-3">
-
-                    {/* Theme Toggle */}
                     <button onClick={() => dispatch(toggleTheme())} className="size-8 flex items-center justify-center bg-white dark:bg-zinc-800 shadow rounded-lg transition hover:scale-105 active:scale-95">
                         {
                             theme === "light"
@@ -42,8 +36,7 @@ const Navbar = ({ setIsSidebarOpen }) => {
                                 : (<SunIcon className="size-4 text-yellow-400" />)
                         }
                     </button>
-
-                    {/* User Button */}
+                    
                     <UserButton />
                 </div>
             </div>
